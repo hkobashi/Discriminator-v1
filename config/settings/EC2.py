087b9c497ebbb3f11c1d189ad70711e4a854cb2a
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-01-23 23:14:53 +0900

    不要ファイル削除

--- config/settings/EC2.py
+++ config/settings/EC2.py
@@ -14,22 +14,3 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 DATABASES = {
     'default':env.db(),
 }
-
-# SECRET_KEY = '86gi2mhyw(trf-&k3ag*b@)ca7(w_r**zcl^o@us)a0iw7-yv$'
-
-
-# ALLOWED_HOSTS = ['staging-v1.discriminator.link', '.staging-v1.discriminator.link']
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Discriminator_staging_v1',
-#         'USER': 'disc_staging',
-#         'PASSWORD': 'CR9BZJ4AP9ZxJ8j',
-#         'HOST': 'discriminator-db.cwcfv6dzfauo.ap-northeast-1.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'TEST': {
-#             'NAME': 'test_Discriminator_staging_v1',
-#         },
-#     }
-# }
