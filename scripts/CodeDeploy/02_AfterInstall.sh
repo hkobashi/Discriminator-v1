5850ec273353a8c2d4a488fcd5ecabbae9657819
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-01-23 16:54:47 +0900

    仮想環境のファイルパス変更
    # what
    - リポジトリと同じ階層に仮想環境のファイルを配置

--- scripts/CodeDeploy/02_AfterInstall.sh
+++ scripts/CodeDeploy/02_AfterInstall.sh
@@ -2,7 +2,7 @@
 source /home/ubuntu/.profile
 
 # 仮想環境起動
-source /home/ubuntu/venv/bin/activate
+source /home/ubuntu/discriminator/venv/bin/activate
 
 # pythonライブラリインストール
 sudo pip install -r /home/ubuntu/discriminator/requirement/base.txt
