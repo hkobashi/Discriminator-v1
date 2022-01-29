5fda7072cccb602a5310b57056d09b16d3460e9c
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-01-29 15:54:51 +0900

    buildspec.yml更新

--- scripts/CodeBuild/seed.sql
+++ scripts/CodeBuild/seed.sql
@@ -1,6 +1,6 @@
 -- codebuildでインストールしたDBに投入するクエリ
-CREATE database Discriminator_dev_v1;
-USE Discriminator_dev_v1;
-CREATE USER 'Disc'@'localhost' IDENTIFIED BY 'V9reamWwANyYc8j';
-GRANT ALL PRIVILEGES ON Discriminator_dev_v1 . * TO 'Disc'@'localhost';
+CREATE database Discriminator_test_v1;
+USE Discriminator_test_v1;
+CREATE USER 'Disc_test'@'localhost' IDENTIFIED BY 'V!9r<eamWwANyY}c8j';
+GRANT ALL PRIVILEGES ON Discriminator_test_v1 . * TO 'Disc_test'@'localhost';
 FLUSH PRIVILEGES;
