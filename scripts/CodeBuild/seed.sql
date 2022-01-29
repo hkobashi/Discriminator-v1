f63fe01666707e2c0cccd6336c225cd8a7bcdcc9
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-01-29 16:36:06 +0900

    Update seed.sql
    # what
    Disc_testユーザーに全権限を付与

--- scripts/CodeBuild/seed.sql
+++ scripts/CodeBuild/seed.sql
@@ -2,5 +2,5 @@
 CREATE database Discriminator_test_v1;
 USE Discriminator_test_v1;
 CREATE USER 'Disc_test'@'localhost' IDENTIFIED BY 'V!9r<eamWwANyY}c8j';
-GRANT ALL PRIVILEGES ON Discriminator_test_v1 . * TO 'Disc_test'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'Disc_test'@'localhost';
 FLUSH PRIVILEGES;
