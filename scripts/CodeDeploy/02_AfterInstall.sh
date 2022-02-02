a22626b6d9182b08c5a3f5cc8ee4006c74cc3162
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-02-02 23:39:16 +0900

    Pythonライブラリ再インストールするシェルを追加

--- scripts/CodeDeploy/02_AfterInstall.sh
+++ scripts/CodeDeploy/02_AfterInstall.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -e
-source /home/ubuntu/.profile
-
-# 仮想環境起動
-source /home/ubuntu/discriminator/venv/bin/activate
+#!/bin/bash
+# 仮想環境削除
+rm -rf /home/ubuntu/discriminator/venv
+virtualenv /home/ubuntu/discriminator/venv
 
 # pythonライブラリインストール
 pip install -r /home/ubuntu/discriminator/requirement/base.txt
+
+# 仮想環境起動
+source /home/ubuntu/discriminator/venv/bin/activate
 deactivate
--- scripts/get_coverage.sh
+++ scripts/get_coverage.sh
@@ -2,3 +2,8 @@
 coverage run manage.py test
 coverage html
 open htmlcov/index.html
+
+#RDS情報(マスターユーザ)
+#disc_v1
+#khN3F6UQYxJvfPXVxLj7
+#discriminator-v1-db.cwcfv6dzfauo.ap-northeast-1.rds.amazonaws.com
