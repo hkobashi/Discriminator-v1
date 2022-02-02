cf92d99d5e6ce2a792f83156ebe03d9cd8ed8d7e
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-02-03 00:05:17 +0900

    Update 02_AfterInstall.sh
    仮想環境を起動する順番を変更

--- scripts/CodeDeploy/02_AfterInstall.sh
+++ scripts/CodeDeploy/02_AfterInstall.sh
@@ -3,9 +3,9 @@
 rm -rf /home/ubuntu/discriminator/venv
 virtualenv /home/ubuntu/discriminator/venv
 
-# pythonライブラリインストール
-pip install -r /home/ubuntu/discriminator/requirement/base.txt
-
 # 仮想環境起動
 source /home/ubuntu/discriminator/venv/bin/activate
+
+# pythonライブラリインストール
+pip install -r /home/ubuntu/discriminator/requirement/base.txt
 deactivate
