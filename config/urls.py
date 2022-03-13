f19977fe42371f6f2815cc1dd3d6a51fd2f3c6f0
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-03-13 17:52:54 +0900

    検索機能実装
    # what
    - タイトル、本文で検索する機能を実装
    ※複数キーワードには非対応
    - indexテンプレートを微修正
    - レビューの参照以外はユーザー認証を必須に変更

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 #    path('admin/', admin.site.urls),
     path('review/', include('review.urls')),
     path('account/', include('allauth.urls')),
-    path('status/', lambda request: HttpResponse('ok')),
+    path('elbHealthCheck/', lambda request: HttpResponse('ok'), name="elb_healthcheck"),
 ]
--- review/views.py
+++ review/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Review
-from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
-
-# templates/403.htmlを返す
-
+from django.contrib import messages
+from django.db.models import Q
 from .forms import ReviewForm
 
 def index(request):
   review_list = Review.objects.all()
   template = "review/index.html"
+  # 検索機能
+  keyword = request.GET.get('keyword')
+  if keyword:
+    result = review_list.filter(
+      # タイトル、本文で検索
+      Q(title__icontains=keyword) |
+      Q(body__icontains=keyword)
+    )
+    messages.success(request, '「{}」の検索結果'.format(keyword))
+    # 検索にヒットしたもののみ表示
+    return render(request, template, {'review_list': result})
+
   return render(request, template, {'review_list': review_list})
 
-@login_required
 def show(request, review_id):
   review = get_object_or_404(Review, pk=review_id)
   template = "review/show.html"
   return render(request, template, {'review': review})
-  # if review.is_published:
-  #   return render(request, template, {'review': review})
-  # else:
-  #   raise PermissionDenied
 
+@login_required
 def create(request):
   if request.method == "POST":
     form = ReviewForm(request.POST)
@@ -32,6 +38,7 @@ def create(request):
   form = ReviewForm
   return render(request, "review/create.html", {'form': form})
 
+@login_required
 def update(request, review_id):
   review = get_object_or_404(Review, pk=review_id)
   template = "review/update.html"
@@ -43,6 +50,7 @@ def update(request, review_id):
   form = ReviewForm(instance=review)
   return render(request, template, {'form': form, 'review_id': review_id})
 
+@login_required
 def delete(request, review_id):
   review = get_object_or_404(Review, pk=review_id)
   review.delete()
