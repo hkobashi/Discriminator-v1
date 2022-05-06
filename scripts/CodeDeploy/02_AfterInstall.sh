a7f046ce7264bac2c33468804b6bd6bb6e81c5b6
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-05-06 18:33:23 +0900

    シェル追加
    同hooksを実行できるかテスト

--- scripts/CodeDeploy/02_AfterInstall.sh
+++ scripts/CodeDeploy/02_AfterInstall.sh
@@ -7,3 +7,7 @@ source /home/ubuntu/discriminator/venv/bin/activate
 # pythonライブラリインストール
 pip install -r /home/ubuntu/discriminator/requirement/base.txt
 deactivate
+
+# Nginx, gunicorn再起動
+sudo systemctl restart nginx
+sudo systemctl restart gunicorn
